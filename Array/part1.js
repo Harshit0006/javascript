60f19268cd73001a1ccaf3e90332aca609299425
Harshit Sharma <harshitsharma20006@gmail.com>
2026-01-06 18:05:28 +0530

    array

---
+++ Array/part1.js
@@ -0,0 +1,12 @@
+const myArray = [1 , 2 , 3 , 4, 5] 
+
+console.log(myArray);
+console.log(myArray[4]);
+
+
+myArray.pop()
+myArray.push(8);
+myArray.unshift(99);
+myArray.shift()
+
+console.log(myArray)
