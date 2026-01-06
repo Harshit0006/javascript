efe15b709ca9ad5bf3dd759039af67859744ff40
Harshit Sharma <harshitsharma20006@gmail.com>
2026-01-06 18:55:18 +0530

    object

--- Array/part1.js
+++ Array/part1.js
@@ -1,5 +1,11 @@
 const myArray = [1 , 2 , 3 , 4, 5] 
 
+const myFriends = ["Anuj" , "Amit" , "Rudra" , "Mayank"]
+const myBroFriends = ["Rahul" , "Ekant" , "Abhinav" ]
+
+const allFriends = [...myFriends , ...myBroFriends]
+
+
 console.log(myArray);
 console.log(myArray[4]);
 
@@ -8,5 +14,6 @@ myArray.pop()
 myArray.push(8);
 myArray.unshift(99);
 myArray.shift()
-
+ 
 console.log(myArray)
+console.log(allFriends)
---
+++ Object/part1.js
@@ -0,0 +1,13 @@
+const myUser = {
+    name : "hero",
+    age : 100,
+    location : "city "
+    
+}
+
+console.log(myUser)
+console.log(myUser.age)
+console.log(Object.keys(myUser))
+console.log(Object.values(myUser))
+console.log(Object.entries(myUser))
+
