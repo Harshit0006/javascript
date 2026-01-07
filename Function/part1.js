d29edc0ae48c3f883b1f9bb036e1c639c8d9db87
Harshit Sharma <harshitsharma20006@gmail.com>
2026-01-07 19:01:15 +0530

    funcation

---
+++ Function/part1.js
@@ -0,0 +1,17 @@
+function functionName(){
+    console.log("hello everyone my name is harshit sharma ")
+}
+functionName()
+
+function addTwoNumber( num1 , num2) {
+   let result = num1 + num2
+  console.log(result)
+    
+}
+addTwoNumber(2 ,4)
+
+
+function addTwoNum(number1 , number2){
+    return number1 + number2
+}
+console.log(addTwoNum(2 ,4))
