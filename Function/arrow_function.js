abc0b33f149d1f2d21e3bc4aea367ec98e85c711
Harshit Sharma <harshitsharma20006@gmail.com>
2026-01-09 15:40:46 +0530

    forLoop

---
+++ Function/arrow_function.js
@@ -0,0 +1,12 @@
+const add = () => {
+
+}
+const addTwo = (num1 , num2) => {
+  return num1 + num2
+}
+
+console.log(addTwo(2 ,4))
+
+const addTwoNum = (number1 , number2) =>  number1 + number2
+
+console.log(addTwo(3 ,5))
--- Function/part1.js
+++ Function/part1.js
@@ -1,17 +1,42 @@
-function functionName(){
+function functionName() {
     console.log("hello everyone my name is harshit sharma ")
 }
 functionName()
 
-function addTwoNumber( num1 , num2) {
-   let result = num1 + num2
-  console.log(result)
-    
+// function 1st type 
+function addTwoNumber(num1, num2) {
+    let result = num1 + num2
+    console.log(result)
+
 }
-addTwoNumber(2 ,4)
+addTwoNumber(2, 4)
 
 
-function addTwoNum(number1 , number2){
+// function 2nd type 
+
+function addTwoNum(number1, number2) {
     return number1 + number2
 }
-console.log(addTwoNum(2 ,4))
+console.log(addTwoNum(2, 4))
+
+
+// function with objects
+
+ const user = {
+    name: "harshit",
+    age: "24",
+    professional: "frontend developer"
+}
+
+
+ function myData(anyObject) {
+    return `${anyObject.name} is logged In`
+}
+console.log(myData(user));
+
+
+ function myData({ name }) {
+  return `${name} is logged In`
+}
+
+console.log(myData(user))
